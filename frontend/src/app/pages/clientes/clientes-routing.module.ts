48b79f2073cf2ff058c36822952809aba3acd553
Site13 Soluções Digitais <tulio.nasc95@gmail.com>
2025-06-11 16:28:45 -0300

    Criação da Rota de Clientes

---
+++ frontend/src/app/pages/clientes/clientes-routing.module.ts
@@ -0,0 +1,23 @@
+// Importação dos módulos necessários do Angular
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ListarComponent } from './listar/listar.component';
+import { NovoComponent } from './novo/novo.component';
+import { EditarComponent } from './editar/editar.component';
+
+// Definição das rotas para o módulo de clientes
+const routes: Routes = [
+  { path: '', component: ListarComponent },
+  { path: 'novo', component: NovoComponent },
+  { path: ':id/editar', component: EditarComponent },
+];
+
+// Módulo de roteamento para o módulo de clientes
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+
+// Exportação do módulo de roteamento
+export class ClientesRoutingModule { }
